b3ee72dc4d61b1bc1ccd2359ec065a9c36e0c08e
alexdaly <alexdaly@gmail.com>
2017-01-04 09:51:42 -0500

    Initial commit.

---
+++ src/app/app.module.ts
@@ -0,0 +1,41 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { routing } from './app.routes';
+// import { MomentModule } from 'angular2-moment';
+
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { StoriesComponent } from './stories/stories.component';
+import { FooterComponent } from './footer/footer.component';
+import { ItemComponent } from './item/item.component';
+
+import { HackerNewsAPIService } from './hackernews-api.service';
+import { ItemCommentsComponent } from './item-comments/item-comments.component';
+import { CommentTreeComponent } from './comment-tree/comment-tree.component';
+import { CommentComponent } from './comment/comment.component';
+// import { DomainPipe } from './domain.pipe';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    StoriesComponent,
+    FooterComponent,
+    ItemComponent,
+    ItemCommentsComponent,
+    CommentTreeComponent,
+    CommentComponent,
+
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    routing
+  ],
+  providers: [HackerNewsAPIService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
